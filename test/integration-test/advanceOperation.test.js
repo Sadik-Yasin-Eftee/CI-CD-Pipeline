88383509b9981d9675e4d534f119ba0fd99e0570
Sadik Yasin Eftee <sadikyasin492@gmail.com>
2024-05-09 00:52:18 +0600

    test case values fixed

--- test/integration-test/advanceOperation.test.js
+++ test/integration-test/advanceOperation.test.js
@@ -2,10 +2,10 @@ const calculator = require("../../src/advanceOperation");
 
 describe('Integration Testing', () => {
     describe.each([
-        [2, 3, 3, 2],
-        [5, 4, 4, 1],
-        [3, 12, 12, 0],
-        [4, 6, 6, 4]
+        [2, 3, 8, 2],
+        [5, 4, 625, 1], 
+        [3, 12, 531441, 3], 
+        [4, 6, 4096, 4] 
     ])('pow(a, b) and modulo(a, b)', (a, b, powExpected, moduloExpected) => {
         test(`pow(${a}, ${b}) and modulo(${a}, ${b})`, () => {
             const powResult = calculator.pow(a, b);
